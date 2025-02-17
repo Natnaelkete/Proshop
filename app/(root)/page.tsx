e2339bc9c63e4ab2b02122d08e8c0ab54d7001a4
Natnaelkete <ketemanatnael023@gmail.com>
2025-02-17 13:47:13 -0800

    add layout and page fils

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
 import { Button } from '@/components/ui/button';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 const Homepage = () => {
   return <Button>What the fuck</Button>;
