4f0b7b23343ccb2d2e8dd0e60dcd3d83fb8c98ca
Natnaelkete <ketemanatnael023@gmail.com>
2025-02-24 20:28:07 -0800

    add seed data to db and fetch data from db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import ProductList from '@/components/shared/product/product-list';
 import { Button } from '@/components/ui/button';
 import sampleData from '@/db/sample-data';
+import { getLatestProduct } from '@/lib/actions/product.actions';
 
-const Homepage = () => {
-  return (
-    <ProductList data={sampleData.products} title='Newest Arrival' limit={4} />
-  );
+const Homepage = async () => {
+  const sampleData = await getLatestProduct();
+  return <ProductList data={sampleData} title='Newest Arrival' limit={4} />;
 };
 
 export default Homepage;
