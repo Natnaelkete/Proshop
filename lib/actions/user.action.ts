173c5de6e6c4c8467100d9a5dee1157b3e3a657e
Natnaelkete <ketemanatnael023@gmail.com>
2025-03-12 16:04:42 -0700

    add Authentication with Next-auth and sign-In form

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { SignInFormSchema } from '../validators';
+import { signIn, signOut } from '@/auth';
+import { isRedirectError } from 'next/dist/client/components/redirect';
+
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = SignInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+
+    await signIn('credentials', user);
+
+    return { success: true, message: 'Sign in successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: 'Invalid email or password' };
+  }
+}
+
+export async function signOutUser() {
+  await signOut();
+}
--- lib/validators.ts
+++ lib/validators.ts
@@ -10,14 +10,19 @@ const currency = z
 
 // Schema ofr inserting product
 export const insertProductSchema = z.object({
-  name: z.string().min(3, 'Name must e at least 3 characters'),
-  slug: z.string().min(3, 'Slug must e at least 3 characters'),
-  category: z.string().min(3, 'Category must e at least 3 characters'),
-  brand: z.string().min(3, 'Brand must e at least 3 characters'),
-  description: z.string().min(3, 'Description must e at least 3 characters'),
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  slug: z.string().min(3, 'Slug must be at least 3 characters'),
+  category: z.string().min(3, 'Category must be at least 3 characters'),
+  brand: z.string().min(3, 'Brand must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters'),
   stock: z.coerce.number(),
   images: z.array(z.string()).min(1, 'Product must have at least one image'),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
+});
+
+export const SignInFormSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 });
