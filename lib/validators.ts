0a0a6f9c7a367127fe8ddf7613300c1cb6e3f645
Natnaelkete <ketemanatnael023@gmail.com>
2025-04-14 01:17:23 -0700

    add order schema and validator

--- lib/validators.ts
+++ lib/validators.ts
@@ -80,3 +80,27 @@ export const paymentMethodSchema = z
     path: ['type'],
     message: 'Invalid payment method',
   });
+
+// Schema for order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, 'User id is required'),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  shippingAddress: shippingAddressSchema,
+  paymentMethodSchema: z
+    .string()
+    .refine((data) => PAYMENT_METHODS.includes(data), {
+      message: 'Invalid payment method',
+    }),
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  price: currency,
+  qty: z.number(),
+});
